c0d0b3957c8402ee1a17aab8133729d5297a2bf3
Kevin <kevinstein@gmx.net>
2024-07-17 14:40:01 +0200

    add Update function

--- pages/create.js
+++ pages/create.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form.js";
 import { StyledLink } from "../components/StyledLink.js";
-import useSWR from "swr";
+
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
@@ -11,28 +11,19 @@ const StyledBackLink = styled(StyledLink)`
 
 export default function CreatePlacePage() {
   const router = useRouter();
-  const { mutate } = useSWR("/api/places");
 
-  async function addPlace(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const placeData = Object.fromEntries(formData);
-
+  async function addPlace(place) {
     const response = await fetch("/api/places", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(placeData),
+      body: JSON.stringify(place),
     });
 
     if (response.ok) {
-      mutate();
       router.push("/");
     }
-
-    event.target.reset();
   }
 
   return (
