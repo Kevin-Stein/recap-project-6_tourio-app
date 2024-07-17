7f9a6513bad35890347f33781f63b72addaa1cdd
Kevin <kevinstein@gmx.net>
2024-07-17 13:56:02 +0200

    add Create Form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,5 @@
 import dbConnect from "../../../db/connect";
-import  Place  from "@/db/models/Place";
+import Place from "@/db/models/Place";
 
 export default async function handler(request, response) {
   await dbConnect();
@@ -7,5 +7,16 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const places = await Place.find();
     return response.status(200).json(places);
+  }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      return response.status(201).json({ status: "Place created." });
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
